7aa70171e3524c94bb14774da63eb230f468d89c
Sandro Padin <sandropadin@gmail.com>
2015-05-13 22:13:40 -0500

    Move logs where they make more sense

--- cloner/cloner.go
+++ cloner/cloner.go
@@ -9,9 +9,9 @@ import (
 func Clone(source locator.Locator, destination locator.Locator, log logger.Logger) {
 	command := commander.CreateCommand("git", "clone", "--depth=1", source.Location(), destination.Location())
 
+	log.Info("Cloning source from %s to %s", source.Location(), destination.Location())
+
 	if err := command.Run(); err != nil {
 		log.Error("Error cloning repo %s", err)
 	}
-
-	log.Info("Cloned source from %s to %s", source.Location(), destination.Location())
 }
--- main.go
+++ main.go
@@ -12,19 +12,23 @@ var log = logger.CreateLogger("main")
 
 func createTempDirectory() locator.Locator {
 	randomizer := randomizer.CreateRandomizer()
-	locator := locator.NewTempDirectoryLocator(randomizer)
+	directory := locator.NewTempDirectoryLocator(randomizer)
 
-	command := commander.CreateCommand("mkdir", "-p", locator.Location())
+	command := commander.CreateCommand("mkdir", "-p", directory.Location())
+
+	log.Info("Creating temporary directory %s", directory.Location())
 
 	if err := command.Run(); err != nil {
 		log.Error("Error creating directory %s", err)
 	}
 
-	return locator
+	return directory
 }
 
 func removeTempDirectory(directory locator.Locator) {
 	command := commander.CreateCommand("rm", "-rf", directory.Location())
+
+	log.Info("Removing temporary directory %s", directory.Location())
 
 	if err := command.Run(); err != nil {
 		log.Error("Error removing directory %s", err)
@@ -36,13 +40,10 @@ type tempDirectoryCallback func(l locator.Locator)
 func withTemporaryDirectory(fn tempDirectoryCallback) {
 	temporaryDirectory := createTempDirectory()
 
-	log.Info("Created temporary directory %s", temporaryDirectory.Location())
-
 	fn(temporaryDirectory)
 
 	removeTempDirectory(temporaryDirectory)
 
-	log.Info("Removed temporary directory %s", temporaryDirectory.Location())
 }
 
 func main() {
@@ -50,8 +51,6 @@ func main() {
 	// destination := locator.NewGitLocator("git@github.com:site-builder/frontend.git")
 
 	withTemporaryDirectory(func(temp locator.Locator) {
-		log.Info("About to clone from: %s", source.Location())
-
 		cloner.Clone(source, temp, logger.CreateLogger("cloner"))
 		// builder.Build(temp, logger.CreateLogger("builder"))
 		// deployer.Deploy(temp, destination, logger.CreateLogger("deployer"))

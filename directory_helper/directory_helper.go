6a91474f185a3a3369a064bcfd4342ed5ca4a70d
Sandro Padin <sandropadin@gmail.com>
2015-05-22 15:02:42 -0500

    Extract directory_helper package

--- directory_helper/directory_helper.go
+++ directory_helper/directory_helper.go
@@ -2,22 +2,52 @@ package directory_helper
 
 import (
 	"github.com/site-builder/fetcher/locator"
+	"github.com/site-builder/fetcher/logger"
+	"github.com/site-builder/fetcher/randomizer"
 	"github.com/site-builder/fetcher/runner"
 )
 
-type callback func(locator.Locator)
+var log = logger.CreateLogger("directory_helper")
+
+type locatorCallback func(locator.Locator)
 
 type DirectoryHelper interface {
-	WithTemporaryDirectory(callback)
+	WithTemporaryDirectory(locatorCallback)
 }
 
 type directoryHelper struct {
 	runner runner.Runner
 }
 
-func NewDirectoryHelper() DirectoryHelper {
-	return &directoryHelper{}
+func NewDirectoryHelper(runner runner.Runner) DirectoryHelper {
+	return &directoryHelper{runner: runner}
 }
 
-func (directoryHelper *directoryHelper) WithTemporaryDirectory(callback callback) {
+func (directoryHelper *directoryHelper) createTempDirectory() locator.Locator {
+	randomizer := randomizer.CreateRandomizer()
+	directory := locator.NewTempDirectoryLocator(randomizer)
+
+	log.Info("Creating temporary directory %s", directory.Location())
+
+	if err := directoryHelper.runner.Run("mkdir", "-p", directory.Location()); err != nil {
+		log.Error("Error creating directory %s", err)
+	}
+
+	return directory
+}
+
+func (directoryHelper *directoryHelper) removeTempDirectory(directory locator.Locator) {
+	log.Info("Removing temporary directory %s", directory.Location())
+
+	if err := directoryHelper.runner.Run("rm", "-rf", directory.Location()); err != nil {
+		log.Error("Error removing directory %s", err)
+	}
+}
+
+func (directoryHelper *directoryHelper) WithTemporaryDirectory(callback locatorCallback) {
+	temporaryDirectory := directoryHelper.createTempDirectory()
+
+	callback(temporaryDirectory)
+
+	directoryHelper.removeTempDirectory(temporaryDirectory)
 }
--- main.go
+++ main.go
@@ -2,54 +2,18 @@ package main
 
 import (
 	"github.com/site-builder/fetcher/cloner"
+	"github.com/site-builder/fetcher/directory_helper"
 	"github.com/site-builder/fetcher/locator"
-	"github.com/site-builder/fetcher/logger"
-	"github.com/site-builder/fetcher/randomizer"
 	"github.com/site-builder/fetcher/runner"
 )
-
-var log = logger.CreateLogger("main")
-
-func createTempDirectory() locator.Locator {
-	randomizer := randomizer.CreateRandomizer()
-	directory := locator.NewTempDirectoryLocator(randomizer)
-	runner := runner.NewRunner()
-
-	log.Info("Creating temporary directory %s", directory.Location())
-
-	if err := runner.Run("mkdir", "-p", directory.Location()); err != nil {
-		log.Error("Error creating directory %s", err)
-	}
-
-	return directory
-}
-
-func removeTempDirectory(directory locator.Locator) {
-	runner := runner.NewRunner()
-
-	log.Info("Removing temporary directory %s", directory.Location())
-
-	if err := runner.Run("rm", "-rf", directory.Location()); err != nil {
-		log.Error("Error removing directory %s", err)
-	}
-}
-
-type tempDirectoryCallback func(l locator.Locator)
-
-func withTemporaryDirectory(fn tempDirectoryCallback) {
-	temporaryDirectory := createTempDirectory()
-
-	fn(temporaryDirectory)
-
-	removeTempDirectory(temporaryDirectory)
-}
 
 func main() {
 	runner := runner.NewRunner()
 	source := locator.NewGitLocator("/tmp/frontend")
+	directoryHelper := directory_helper.NewDirectoryHelper(runner)
 	// destination := locator.NewGitLocator("git@github.com:site-builder/frontend.git")
 
-	withTemporaryDirectory(func(temp locator.Locator) {
+	directoryHelper.WithTemporaryDirectory(func(temp locator.Locator) {
 		cloner.Clone(runner, source, temp)
 		// builder.Build(runner, temp)
 		// deployer.Deploy(runner, temp)

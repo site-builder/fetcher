438a1bb8a48bf3d42217a71f7b2ff961a80362d3
Sandro Padin <sandropadin@gmail.com>
2015-05-25 14:03:52 -0500

    Log cloned branch name

--- cloner/cloner.go
+++ cloner/cloner.go
@@ -9,8 +9,8 @@ import (
 var log = logger.CreateLogger("cloner")
 
 func Clone(runner runner.Runner, source locator.Locator, destination locator.Locator) {
-	log.Info("Cloning source from %s to %s", source.Location(), destination.Location())
 	branch := source.Metadata()["branch"]
+	log.Info("Cloning source from %s@%s to %s", source.Location(), branch, destination.Location())
 
 	if err := runner.Run("git", "clone", "-b", branch, "--single-branch", "--depth=1", source.Location(), destination.Location()); err != nil {
 		log.Error("Error cloning repo %s", err)

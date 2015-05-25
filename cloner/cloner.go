0f5222784d8d7710956bd0b284a24faa8376f19d
Sandro Padin <sandropadin@gmail.com>
2015-05-25 13:46:30 -0500

    Cloner can checkout specific branch

--- cloner/cloner.go
+++ cloner/cloner.go
@@ -10,9 +10,11 @@ var log = logger.CreateLogger("cloner")
 
 func Clone(runner runner.Runner, source locator.Locator, destination locator.Locator) {
 	log.Info("Cloning source from %s to %s", source.Location(), destination.Location())
+	branch := source.Metadata()["branch"]
 
-	if err := runner.Run("git", "clone", "-b", "master", "--single-branch", "--depth=1", source.Location(), destination.Location()); err != nil {
+	if err := runner.Run("git", "clone", "-b", branch, "--single-branch", "--depth=1", source.Location(), destination.Location()); err != nil {
 		log.Error("Error cloning repo %s", err)
+
 		if message := err.Error(); message == "exit status 128" {
 			log.Info("Does the repo you're trying to clone exist?")
 		}

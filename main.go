f6abdf5f3772b8454bce30a91eef95c5d247fe55
Sandro Padin <sandropadin@gmail.com>
2015-06-05 16:13:23 -0500

    Initial working version

--- main.go
+++ main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"github.com/site-builder/worker/builder"
 	"github.com/site-builder/worker/cloner"
+	"github.com/site-builder/worker/deployer"
 	"github.com/site-builder/worker/directory_helper"
 	"github.com/site-builder/worker/locator"
+	"github.com/site-builder/worker/logger"
 	"github.com/site-builder/worker/runner"
+
+	"flag"
 )
 
-func main() {
-	runner := runner.NewRunner()
-	source := locator.NewGitLocator("/tmp/frontend")
-	cloner := cloner.NewCloner(runner)
-	directoryHelper := directory_helper.NewDirectoryHelper(runner)
-	// destination := locator.NewGitLocator("git@github.com:site-builder/frontend.git")
+var log = logger.CreateLogger("worker")
 
-	directoryHelper.WithTemporaryDirectory(func(tmp locator.Locator) {
-		cloner.Clone(source, tmp)
-		// builder.Build(runner, tmp)
-		// deployer.Deploy(runner, tmp)
+func main() {
+	var sourceRepo string
+	var sourceBranch string
+
+	var destinationRepo string
+	var destinationBranch string
+
+	flag.StringVar(&sourceRepo, "source-repo", "", "repo to clone")
+	flag.StringVar(&sourceBranch, "source-branch", "master", "branch to clone")
+
+	flag.StringVar(&destinationRepo, "destination-repo", "", "repo to send deploy")
+	flag.StringVar(&destinationBranch, "destination-branch", "gh-pages", "branch to send deploy")
+
+	flag.Parse()
+
+	log.Info("Starting build")
+
+	source := locator.NewGitLocatorWithBranch(sourceRepo, sourceBranch)
+	destination := locator.NewGitLocatorWithBranch(destinationRepo, destinationBranch)
+
+	runner := runner.NewRunner()
+	directoryHelper := directory_helper.NewDirectoryHelper(runner)
+	cloner := cloner.NewCloner(runner)
+	builder := builder.NewBuilder(runner)
+	deployer := deployer.NewDeployer(runner)
+
+	directoryHelper.WithTemporaryDirectory(func(tmpSource locator.Locator) {
+		directoryHelper.WithTemporaryDirectory(func(tmpDestination locator.Locator) {
+			cloner.Clone(source, tmpSource)
+			builder.Build(tmpSource, tmpDestination)
+			deployer.Deploy(tmpDestination, destination)
+		})
 	})
+
+	log.Info("Build complete")
 }
